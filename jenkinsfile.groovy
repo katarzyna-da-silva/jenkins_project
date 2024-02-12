4063428cbd0171414f752748176a590bf73e6322
katarzyna-da-silva <conic916@gmail.com>
2024-02-12 23:16:17 +0100

    jenkinsfile.groovy

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -9,7 +9,7 @@ pipeline {
     stages {                  // code section
         stage('Build') {               // preparation env.
             steps {          
-                sh "apt-get update && apt-get install -y  python${PYTHON_VERSION} python${PYTHON_VERSION}-dev python3-pip"          //  install python
+                sh "sudo apt-get update && sudo apt-get install -y  python${PYTHON_VERSION} python${PYTHON_VERSION}-dev python3-pip"          //  install python
                 sh "pip3 install virtualenv"                                                                                        // install environment 
                 sh "virtualenv -p = python${PYTHON_VERSION} python${VENV_NAME}"                                                       // install version & name env
                 sh "source ${VENV_NAME}/bin/activate"                                                                                // active env
